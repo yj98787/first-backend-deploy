a0c9d411f97656bebf1e01605ac050fa3bbf095b
yj98787 <yj98787@gmail.com>
2025-10-29 20:31:57 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const Port = 4000
+const PORT = process.env.PORT || 4000
 
 app.get('/',(req,res)=>{
     res.send("Welcome to my first api");
@@ -19,6 +19,6 @@ app.get('/chai',(req,res)=>{
     res.send("<h2>Chai Aur Code</h2>")
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server listining at ${process.env.PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server listining at ${PORT}`);
 })
