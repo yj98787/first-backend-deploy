26fcf5ddc7fd45c31a57e2d80555afeaabcebae4
yj98787 <yj98787@gmail.com>
2025-10-29 19:59:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Port = 4000
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to my first api");
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send("yashdotcom");
+})
+
+app.get('/login',(req,res)=>{
+    res.send("<h1>Login at Chai aur Code</h1>")
+})
+
+app.get('/chai',(req,res)=>{
+    res.send("<h2>Chai Aur Code</h2>")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server listining at ${process.env.PORT}`);
+})
